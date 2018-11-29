1c005a21ca7999b27044f96c61740be548e98e97
kcnnn <kcenna@gmail.com>
2018-11-29 16:23:45 -0600

    Add files via upload

--- products.js
+++ products.js
@@ -18,13 +18,24 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
 
-  connection.query("SELECT * FROM products", function(err, res){
+ connection.query("SELECT * FROM products", function(err, res){
+ //   connection.query("SELECT id FROM products", function(err, res){
+
     if(err) throw err;
-    console.log(res);
+   // console.log(res);
+    console.log("Product" + "Dept" + "  Price" + "  Quantity")
     for(var i = 0; i < res.length; i++){
-      console.log(res[i].product_name);
+      console.log(res[i].product_name + " || " + res[i].department_name + "  ||  " + res[i].price + "  || " + res[i].stock_quantity);
+      console.log("----------------------------------------");
+      //console.log(res[i].department_name);
     }
     connection.end();
   })
 
 });
+
+// function foo(txt){
+//   console.log(txt);
+// }
+
+// foo("ken");
